e12ee6c8d80ac9a1501ceebda8454578da2e8cb9
Всеволод <grinvsevolod1@netnext.site>
2025-09-25 17:16:30 +0300

    111

--- src/app/(site)/(auth)/signin/page.tsx
+++ src/app/(site)/(auth)/signin/page.tsx
@@ -1,20 +1,18 @@
+'use client';
+
 import Signin from "@/components/Auth/SignIn";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "Sign In | Nicktio",
+  title: "Sign In | Nicktio",
 };
 
-const SigninPage = () => {
+export default function SigninPage() {
   return (
     <>
       <Breadcrumb pageName="Sign In Page" />
-
       <Signin />
     </>
   );
-};
-
-export default SigninPage;
+}
